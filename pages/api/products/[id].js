6392b366166425538ca7b82c280afc4642f7f965
Anja Klaßen <anja.klassen@posteo.de>
2024-01-24 17:13:55 +0100

    add solution without bonus

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,13 +1,22 @@
-import { products } from "../../../lib/products";
+import Product from "@/db/models/product";
+import dbConnect from "@/db/connect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
 
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(product);
   }
-
-  response.status(200).json(product);
 }
+
+//To find a single product by its id, you can use the `Product` model and the
+//`.findById()` method: `Product.findById(id)`.
+//- Delete `lib/products.js` because it is not used anymore.
+//;
